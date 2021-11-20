aa68280db9d2afb53b48e0edd020672a007c7e54
dilaram pokhrel <dila414@gmail.com>
2021-11-20 18:11:28 -0500

    test

--- main/java/day3/DataTypesDemo.java
+++ main/java/day3/DataTypesDemo.java
@@ -4,6 +4,11 @@ public class DataTypesDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		String name="Rachana Acharya";
+		System.out.println(a+b);
+		System.out.println(name);
 
 	}
 
--- main/java/day4/MathOpertorDemo.java
+++ main/java/day4/MathOpertorDemo.java
@@ -1,10 +1,11 @@
 package day4;
 
-public class DataStructureDemo {
+public class MathOpertorDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//nknknk
+		double d=2+4*5-8%2-6+9;
+		System.out.println(d);
 
 	}
 
